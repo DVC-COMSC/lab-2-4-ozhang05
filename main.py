01cd3adec8890f74f155d8b38c2b5ecc39c0299f
Oliver <ozhang2305@gmail.com>
2023-06-24 23:26:33 -0700

    fixed

--- main.py
+++ main.py
@@ -5,15 +5,14 @@ def main():
     
     original_str = "Python Programming"
     sub1 = original_str[0: 6]
-    sub2 = original_str[6:]
-    merge_str = sub2 + sub1
+    sub2 = original_str[7:]
+    merge_str = sub2 + " " + sub1
     
     print (sub2)
     print (sub1)
     print (merge_str)
-
+    
     pass
-
 
 if __name__ == '__main__':
     main()
