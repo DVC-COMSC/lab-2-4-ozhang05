8675c5f58a5b6f5ba656d319dada2ac565eac1f1
Kyuwoong Lee <kwooler@gmail.com>
2023-06-25 21:20:29 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    
+    original_str = "Python Programming"
+    sub1 = original_str[0: 6]
+    sub2 = original_str[7:]
+    merge_str = sub2 + " " + sub1
+    
+    print (sub2)
+    print (sub1)
+    print (merge_str)
+    return sub1, sub2, merge_str
     pass
-
 
 if __name__ == '__main__':
     main()
